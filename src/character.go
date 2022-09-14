9a52765a9603e48c3a15dc8a2f1784c19e549fc9
ctaoinfo <tao.chupin@ynov.com>
2022-09-14 15:09:47 +0200

    nouveau format

---
+++ src/character.go
@@ -0,0 +1,34 @@
+package main
+
+type Character struct {
+	name          string
+	class         string
+	level         int
+	health        int
+	currentHealth int
+	inventory     [10]string
+}
+
+func Init(name string, class string, level int, health int, currentHealth int) Character {
+
+	p1 := Character{
+		name:          name,
+		class:         class,
+		level:         level,
+		health:        health,
+		currentHealth: currentHealth,
+		inventory:     [10]string{"potion", "potion", "potion"},
+	}
+	return p1
+
+	// Possibilité d'un deuxième personnage
+
+	// p2 := Character{
+	// 	name: ""
+	// 	class: "Berserker"
+	// 	level: 1
+	// 	health: 100
+	// 	currentHealth: 40
+	// 	inventory: [10]string{"potion", "potion", "potion"}
+	// }
+}
--- src/main.go
+++ src/main.go
@@ -2,3 +2,13 @@
 // Creation 13/09/2022
 // YTrack Ynov Campus
 // RedProject
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(Init("Zaganor", "Devil", 1, 100, 40))
+
+}
